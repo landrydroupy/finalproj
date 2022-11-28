9eb055867ffa86c0e7a868118fd7f605d3d1b321
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-11-28 14:19:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <!doctype html>
 <html lang="en">
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Buildings</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      
+<h1>Movies</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>MovieID</th>
+            <th>Movie Name</th>
+            <th>Director</th>
+           <th>Release Date</th>
+            <th>Rating</th>
+            <th>IMDB Score</th>
+          </tr>
+        </thead>
+        <tbody>
+      
+      
+      
+      
 $servername = "localhost";
 $username = "landryou_user";
 $password = "A2kYbmhiMHTE";
@@ -14,4 +39,34 @@ if ($conn->connect_error) {
 }
 
 
+$sql = "SELECT * from movies";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["movieID"]?></td>
+    <td><?=$row["movieName"]?></td>
+    <td><?=$row["director"]?></td>
+    <td><?=$row["releaseDate"]?></td>
+    <td><?=$row["rating"]?></td>
+    <td><?=$row["imdb_score"]?></td>
+    
+    <td>
+      
+    </td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
 </html>
