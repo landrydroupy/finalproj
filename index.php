9d7c619d85bf557677be0f4e6995f32cfacb30fd
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-04 10:14:42 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("nav.php");?>
+    <title>Courses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+
+  
+  <body>
+    
+  <div class="jumbotron">
+  <h1 class="display-4"> Hello, welcome to MovieTracker </h1>
+  <p class="lead">Through the use of this site, users will be able to input personally watched movies to keep a personalized catalog of the movies they have watched</p>
+  <hr class="my-4">
+  <p>Use the following buttons to begin using the website!</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Input Movie Details</a>
+     <a class="btn btn-primary btn-lg" href="#" role="button">Write a Review</a>
+     <a class="btn btn-primary btn-lg" href="#" role="button">Leave a Plot Summary</a>
+  </p>
+</div>
+  
+  </body>
+  
+  
+  
+  
+  
+  
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
