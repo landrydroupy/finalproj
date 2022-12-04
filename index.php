942cc45744574e8400417d8678ca1e89ee88b948
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-04 13:28:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
   <body>
      </break>
      </break>
+ <div class="container">
   <div class="jumbotron" style="text-align:center">
   <h1 class="display-4">Hello, world!</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
@@ -24,7 +25,7 @@
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </p>
 </div>
-  
+</div>
   </body>
   
   
