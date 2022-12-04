7459edeebd89b56fa0206fca83625eefc70a70d6
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-04 09:48:57 -0600

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,1 +1,14 @@
-
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" href="index.php">MovieTracker</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Movies</a>
+      <a class="nav-item nav-link" href="#">Reviews</a>
+      <a class="nav-item nav-link disabled" href="#">Plot</a>
+    </div>
+  </div>
+</nav>
