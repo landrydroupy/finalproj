ac7d2868ab2c66523daa55df201518a588a3ca38
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-05 11:05:37 -0600

    Create movie-add-save.php

---
+++ movie-add-save.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Movie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "landryou_user";
+$password = "A2kYbmhiMHTE";
+$dbname = "landryou_project_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+ <!--$_POST['nMovieName'],$_POST['nDirector'],$_POST['nReleaseDate'],$_POST['nRating'],$_POST['nIMDBScore'])-->
+$nMovieName = $_POST['nMovieName'];
+$nDirector= $_POST['nDirector'];
+$nReleaseDate= $_POST['nReleaseDate'];
+$nRating= $_POST['nRating'];
+$nIMDBScore= $_POST['nIMDBScore'];
+ 
+    
+$sql = "insert into movies (courseid,fname,lname,grade) value (?,?,?,?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("issi", $sCourseID,$sFname,$sLname,$sGrade);
+    $stmt->execute();
+?>
+    
+    <h1>Add Student</h1>
+<div class="alert alert-success" role="alert">
+  New student added.
+</div>
+    <a href="student.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
