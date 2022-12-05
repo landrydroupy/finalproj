12db6d7ec077250294b26cbdd5a566c832cdcbb0
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-05 13:24:39 -0600

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("nav.php");?>
-    <title>Movies</title>
+    <title>Reviews</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Movies</h1>
+    <h1>Reviews</h1>
 <table class="table table-striped">
   <thead>
     <tr>
