aa20d264d1915985e5e24cf42e3b0adfdaf1ad5c
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-15 15:10:00 -0600

    Update about.php

--- about.php
+++ about.php
@@ -18,7 +18,9 @@
   <div class="description" style="text-align:center; font-family:Dubai; color:black;">
   <h1 class="display-4">About MovieTracker</h1>
  <p class="description">MovieTracker was created with the purpose for people to watch and log movies they love. Through the customizeable tables throughout the website users will be able to input and share their personalized movie databases with everyone!</p>
-  <hr class="my-4">
+    <h2> About the Founder </h2>
+    <p>Hello everyone, my name is Landry Droupy. I created MovieTracker due to watching movies being one of my favorite past times. I wanted to create a way to easily share and store movie information in a website form so that everyone can see and share the different movie experiences they have had.</p>
+ <hr class="my-4">
 </div>
 </div>
   </body>
