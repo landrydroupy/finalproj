c333234a3d099bd626afca0f6b02168cf3438cbb
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-15 17:29:29 -0600

    Create review-cards.php

---
+++ review-cards.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<?php include("nav.php");?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Reviews</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Review Distribution</h1>
+
+<div class="card-group">
+    <?php
+$servername = "localhost";
+$username = "landryou_user";
+$password = "A2kYbmhiMHTE";
+$dbname = "landryou_project_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT numberRating from rate";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  
+  while($row = $result->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["rate"]?>/10</h5>
+      <p class="card-text"><ul>
+<?php
+    $section_sql = "select s.numberRating,r.movieName,r.personalRating from reviews r join rating s on r.personalRating = s.numberRating where s.numberRating=" . $row["personalRating"];
+    $section_result = $conn->query($section_sql);
+    
+    while($section_row = $section_result->fetch_assoc()) {
+      echo "<li>" . $section_row["movieName"] . "</li>";
+    }
+?>
+      </ul></p>
+  </div>
+    </div>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </card-group>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
