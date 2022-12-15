f56c4bbda4a035821def468e24a8b99f06bf8284
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-15 15:07:34 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,24 @@
+<!doctype html>
 
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("nav.php");?>
+    <title>Courses</title>
+    <link rel="stylesheet" href="home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+
+ 
+
+  <body>
+     
+ <div class="container">
+  <div class="description" style="text-align:center; font-family:Dubai; color:black;">
+  <h1 class="display-4">About MovieTracker</h1>
+ <p class="description">MovieTracker was created with the purpose for people to watch and log movies they love. Through the customizeable tables throughout the website users will be able to input and share their personalized movie databases with everyone!</p>
+  <hr class="my-4">
+</div>
+</div>
+  </body>
