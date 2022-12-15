b2e10b64e150203d3c13e514d35374337fcea157
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-15 15:27:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,28 +9,6 @@
     <link rel="stylesheet" href="home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
-
- $servername = "localhost";
-$username = "landryou_user";
-$password = "A2kYbmhiMHTE";
-$dbname = "landryou_project_data";
- 
-  $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  switch ($_POST['saveType']) {
-    case 'Add':
-      $sqlAdd = "insert into movies (movieName,director,releaseDate,rating,imdb_score) value (?,?,?,?,?)";
-      $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("ssssd", $_POST['nMovieName'],$_POST['nDirector'],$_POST['nReleaseDate'],$_POST['nRating'],$_POST['nIMDBScore']);
-      $stmtAdd->execute();
-      echo '<div class="alert alert-success" role="alert">New Movie added.</div>';
-      break;
-
   <body>
      
  <div class="container">
@@ -71,46 +49,6 @@ if ($conn->connect_error) {
 </div>
 </div>
   </body>
-  
-  <div class="modal fade" id="addMovie" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addMovieLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addMovieLabel">Add Movie</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form method="post" action="">
-                <div class="mb-3">
-                 
-                  <!--$_POST['nMovieName'],$_POST['nDirector'],$_POST['nReleaseDate'],$_POST['nRating'],$_POST['nIMDBScore'])-->
-                  <label for="movie_movieName" class="form-label">Movie Name</label>
-                  <input type="text" class="form-control" id="movie_movieName" aria-describedby="nameHelp" name="nMovieName">
-                  <div id="nameHelp" class="form-text">Enter the Movie's Name.</div>
-                  <label for="movie_director" class="form-label">Director Name</label>
-                  <input type="text" class="form-control" id="movie_director" aria-describedby="nameHelp" name="nDirector">
-                  <div id="nameHelp" class="form-text">Enter the Movie's Director</div>
-                  <label for="movie_ReleaseDate" class="form-label">Release Date</label>
-                  <input type="text" class="form-control" id="movie_ReleaseDate" aria-describedby="nameHelp" name="nReleaseDate">
-                  <div id="nameHelp" class="form-text">Enter the Movie's Release Date.</div>
-                  <label for="movie_rating" class="form-label">Rating</label>
-                  <input type="text" class="form-control" id="movie_rating" aria-describedby="nameHelp" name="nRating">
-                  <div id="nameHelp" class="form-text">Enter the Movie's Rating.</div>
-                   <label for="movie_imdbScore" class="form-label">Enter IMDB Score</label>
-                  <input type="text" class="form-control" id="movie_imdbScore" aria-describedby="nameHelp" name="nIMDBScore">
-                  <div id="nameHelp" class="form-text">Enter the Movie's IMDB Score.</div>
-		  
-                </div>
-                <input type="hidden" name="saveType" value="Add">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-  
-  
   
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
