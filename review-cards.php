002626e4ee2f1ca04e5562901e700e4c638b9584
Landry Droupy <78325544+landrydroupy@users.noreply.github.com>
2022-12-15 19:09:16 -0600

    Update review-cards.php

--- review-cards.php
+++ review-cards.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Review Distribution</h1>
+    <h1>Movie Review Distribution</h1>
 
 <div class="card-group">
     <?php
